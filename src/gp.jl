2c688a1001325630507b1bc122ff8a8fb6456a08
jaimerzp <jaimerz011235813@gmail.com>
2022-08-29 17:50:36 +0200

    docs

--- src/gp.jl
+++ src/gp.jl
@@ -1,50 +1,52 @@
+"""
+    marginal_lkl(mean, kernel; data_cov=nothing)
+
+Marginal likelihood implementation of a GP's. 
+This is equivalent to analytically marginalizing over the GP's nodes. 
+Used when GP is linearly related to data. 
+
+Arguments:
+- `meam::Vector{Number}`: GP's mean.
+- `kernel::Matrix{Number}`: GP's covariance matrix
+- `data_cov::Matrix{Float}`: data covariance matrix
+
+Returns:
+- `GP::MvNormal`: instance of MvNormal.
+"""
 function marginal_lkl(mean, kernel; data_cov=nothing)
-    """
-    Marginal likelihood implementation of a GP's. \
-    This is equivalent to analytically marginalizing \
-    over the GP's nodes. \
-    Used when GP is linearly related to data. \
-    Args:
-        mean (Vector{Number}): GP's mean.
-        kernel (Matrix{Number}): GP's cov mat.
-    Kwargs:
-        data_cov (Matrix{Float}): data cov. mat.
-    Returns:
-        GP (MvNormal): instance of MvNormal.
-    """
     if data_cov != nothing
         kernel .+= data_cov
     end
     return MvNormal(mean, kernel)
 end
 
+"""
+    posterior_predict(X_new, X_old, mean_new, mean_old, data, cov_fn;
+                            data_cov=nothing)
+
+Returns a function that transforms the GP 
+from an old parameter space to a new parameter space, 
+given a particular set of values for the GP's covariance 
+matrix hyperparameters. 
+
+Arguments:
+- `new_X::Vector{Float}`: target position vector.
+- `old_X::Vector{Float}`: position vector of the latent GP.
+- `new_mean::Vector{Number}`: expected GP mean for new position vector. 
+- `old_mean::Vector{Number}`: GP's mean in old position vector. 
+- `data::Vector{Float}`: data vector.
+- `cov_fn::function`: covariance function used to generate the GP's kernel. 
+- `data_cov::Matrix{Float}`: data covariance matrix
+
+Returns:
+- `predict()`: predicts GP in target space.
+   Arguments:
+   - `a=a, b=b...`: Cov mat hyperparameters
+   Returns:
+   - `GP::MvNormal`: instance of MvNormal.
+"""
 function posterior_predict(X_new, X_old, mean_new, mean_old, data, cov_fn;
                             data_cov=nothing)
-    """
-    Returns a function that transforms the GP \
-    from an old parameter space to a new parameter space, \
-    given a particular set of values for the GP's covariance \
-    matrix hyperparameters. \
-    Args:
-        new_X (Vector{Float}): target position vector.
-        old_X (Vector{Float}): position vector of the \
-                               latent GP.
-        new_mean (Vector{Number}): expected GP mean \
-                                   for new position vector. 
-        old_mean (Vector{Number}): GP's mean in old \
-                                   position vector. 
-        data (Vector{Float}): data vector.
-        cov_fn (function): covariance function used to generate \
-                           the GP's kernel. 
-    Kwargs:
-        data_cov (Matrix{Float}): data cov. mat.
-    Returns:
-        predict (function): predicts GP in target space.
-            Kwargs:
-                a=a, b=b... : Cov mat hyperparameters
-            Returns:
-                GP (MvNormal): instance of MvNormal.
-    """
     if data_cov == nothing
         data_cov = zeros(length(X_new), length(X_new))
     end
@@ -63,38 +65,44 @@ function posterior_predict(X_new, X_old, mean_new, mean_old, data, cov_fn;
     end
 end
 
+"""
+    latent_GP(mean, nodes, kernel)
+
+Latent variable implementation of a GP's. 
+Rotates the GP ndodes by the covariance 
+matrix and adds them to the mean vector. 
+Used when GP is not linearly related to data. 
+
+Arguments:
+- `mean::Vector{Number}`: GP's mean.
+- `nodes::Vector{Number}`: GP's nodes.
+- `kernel::Matrix{Number}`: GP's cov mat.
+
+Returns:
+- `GP::Vector{Number}`: Gaussian process realization.
+"""
 function latent_GP(mean, nodes, kernel)
-    """
-    Latent variable implementation of a GP's. \
-    Rotates the GP ndodes by the covariance \
-    matrix and adds them to the mean vector. \
-    Used when GP is not linearly related to data. \
-    Args:
-        mean (Vector{Number}): GP's mean.
-        nodes (Vector{Number}): GP's nodes.
-        kernel (Matrix{Number}): GP's cov mat.
-    Returns:
-        GP (Vector{Number}): Gaussian process realization.
-    """
     return mean .+ cholesky(kernel).U' * nodes
 end
 
+"""
+    conditional(old_X, new_X, latent_gp, cov_fn; kwargs...)
+
+Given the GP's covariance matrix, applies a 
+Wiener filter to transform the latent GP 
+N-dimensional parameter space to M-dimensional 
+target parameter space.
+
+Arguments:
+- `old_X::Vector{Float}`: position vector of the latent GP.
+- `new_X::Vector{Float}`: target position vector.
+- `latent_gp::Vector{Number}`: latent GP realization. 
+- `a=a, b=b...` : Covariance matrix hyperparameters.
+
+Returns:
+- `gp::Vector{Number}`: GP in target space. 
+"""
 function conditional(old_X, new_X, latent_gp, cov_fn; kwargs...)
-    """
-    Given the GP's covariance matrix, applies a \
-    Wiener filter to transform the latent GP \
-    N-dimensional parameter space to M-dimensional \
-    target parameter space. \
-    Args:
-        old_X (Vector{Float}): position vector of the \
-                               latent GP.
-        new_X (Vector{Float}): target position vector.
-        latent_gp (Vector{Number}): latent GP realization. 
-    Kwargs:
-        a=a, b=b... : Cov mat hyperparameters 
-    Returns:
-        gp (Vector{Number}): GP in target space. 
-    """
     N = length(new_X)
     Z = [new_X; old_X]
     K = cov_fn(Z; kwargs...)
--- src/kernels.jl
+++ src/kernels.jl
@@ -1,45 +1,54 @@
 """
-Kernel functions used to produce the \
-the covariance matrix of the Gaussian Processes. \
-See https://en.wikipedia.org/wiki/Gaussian_process \
-for details. \
+    sqexp_cov_fn(X; delta=0.0005, kwargs...)
+
+Implementation of the square exponential kernel. 
+
+Arguments:
+
+- `X::Vector{Float}` : node's position vector 
+- `delta::Float` : strength of white noise component for numerical stability. 
+- `eta::Number` : covariance matrix amplitude.
+- `l::Number` : covariance matrix correlation length.
+
+Returns:
+
+- `cov_mat::Matrix` : GP's covariance matrix
+
+Usage:
+
+```julia
+sqexp_cov_fn(x; eta=0.05, l=1.0)
+```
 """
 function sqexp_cov_fn(X; delta=0.0005, kwargs...)
-    """
-    Implementation of the square exponential kernel. \
-    Args:
-        X (Vector{Float}): node's position vector \
-    kwargs: 
-        delta = 0.0005: strength of white noise \
-                        component for \
-                        for numerical stability. 
-        eta (Number): cov. mat. amplitude.
-        l (Number): cov. mat. correlation length.
-    Returns:
-        cov_mat (Matrix (X,X)): GP's cov. mat.
-    """
-    # Squared-exponential covariance function
     X_mat = _turn_into_mat(X)
     D = pairwise(Distances.Euclidean(), X_mat, dims=1)
     return @.(kwargs[:eta] * exp(-D^2 / (2*kwargs[:l]))) + delta * I
 end 
 
+"""
+    exp_cov_fn(X; delta=0.0005, kwargs...)
+
+Implementation of the exponential kernel also known as Ornstein–Uhlenbeck.
+
+Arguments:
+
+- `X::Vector{Float}` : node's position vector 
+- `delta::Float` : strength of white noise component for numerical stability. 
+- `eta::Number` : covariance matrix amplitude.
+- `l::Number` : covariance matrix correlation length.
+
+Returns:
+
+- `cov_mat::Matrix` : GP's covariance matrix
+
+Usage:
+
+```julia
+exp_cov_fn(x; eta=0.05, l=1.0)
+```
+"""
 function exp_cov_fn(X; delta=0.0005, kwargs...)
-    """
-    Implementation of the exponential kernel \
-    also known as Ornstein–Uhlenbeck. \
-    Args:
-        X (Vector{Float}): node's position vector \
-    kwargs: 
-        delta = 0.0005: strength of white noise \
-                        component for \
-                        for numerical stability. 
-        eta (Number): cov. mat. amplitude.
-        l (Number): cov. mat. correlation length.
-    Returns:
-        cov_mat (Matrix (X,X)): GP's cov. mat.
-    """
-    # Exponential covariance function
     X_mat = _turn_into_mat(X)
     D = pairwise(Distances.Euclidean(), X_mat, dims=1)
     return   @.(kwargs[:eta] * exp(-D / kwargs[:l])) + delta * I
