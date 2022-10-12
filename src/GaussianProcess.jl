88fbfa860eae9e3dfe71ff5bdda564f5e6d9d0fb
jaimerzp <jaimerz011235813@gmail.com>
2022-10-12 11:36:41 +0100

    forgot to export

--- src/GaussianProcess.jl
+++ src/GaussianProcess.jl
@@ -1,7 +1,7 @@
 module GaussianProcess
 
 export const_cov_fn, lin_cov_fn, noise_cov_fn, ratquad_cov_fn
-export sin_cov_fn, exp_cov_fn, sqexp_cov_fn 
+export sin_cov_fn, exp_cov_fn, sqexp_cov_fn, sqexp_cov_grad
 export marginal_lkl, latent_GP, conditional, posterior_predict
 
 using Distributions, Distances, LinearAlgebra
